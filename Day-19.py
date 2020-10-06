15cc5a2ec33fad36cac1ab97269f7816ac5ebbf9
Sumanth K <48558948+Sumanth2905@users.noreply.github.com>
2020-10-06 21:46:05 +0530

    Update Day-19.py

--- Day-19.py
+++ Day-19.py
@@ -1,17 +1,10 @@
-class Solution:
-      def __init__(self):
-          self.mystack = list()
-          self.myqueue = list()
-          return(None)
-
-      def pushCharacter(self, char):
-          self.mystack.append(char)
-
-      def popCharacter(self):
-          return(self.mystack.pop(-1))
-
-      def enqueueCharacter(self, char):
-          self.myqueue.append(char)
-
-      def dequeueCharacter(self):
-          return(self.myqueue.pop(0))
+class Calculator(AdvancedArithmetic):
+    def divisorSum(self, n):
+        if n == 1:
+            return 1
+        else:
+            factor_sum = 1 + n 
+            for i in range(2, n//2 + 1):
+                if n % i == 0:
+                    factor_sum += i
+            return factor_sum
